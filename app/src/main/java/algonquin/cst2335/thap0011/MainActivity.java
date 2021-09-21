64105b69c48a383b5024df17f0b2350d383b2792
seemani11 <thap0011@algonquinlive.com>
2021-09-21 13:17:27 -0400

    checkbox, radio and switch added

--- app/src/main/java/algonquin/cst2335/thap0011/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/thap0011/MainActivity.java
@@ -3,6 +3,13 @@ package algonquin.cst2335.thap0011;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mytext = findViewById(R.id.textview);
+        Button mybutton = findViewById(R.id.mybutton);
+        EditText myedit = findViewById(R.id.myedittext);
+
+        CheckBox mycb = findViewById(R.id.cb);
+        Switch mysw = findViewById(R.id.sw);
+        RadioButton myrb =  findViewById(R.id.rb);
+
+        mybutton.setOnClickListener(vw ->{
+
+            String textobtained = myedit.getText().toString();
+            mytext.setText("the text you entered is  " + textobtained);
+        });
+
+        mycb.setOnCheckedChangeListener((btn,isChecked)->{
+            Toast.makeText(getApplicationContext(), "checkbox clicked", Toast.LENGTH_SHORT).show();
+        });
+
+        mysw.setOnCheckedChangeListener((btn,isChecked)->{
+            Toast.makeText(getApplicationContext(), "switch clicked", Toast.LENGTH_SHORT).show();
+        });
+
+        myrb.setOnCheckedChangeListener((btn,isChecked)->{
+            Toast.makeText(getApplicationContext(), "Radiobutton clicked", Toast.LENGTH_SHORT).show();
+        });
     }
+
 }
