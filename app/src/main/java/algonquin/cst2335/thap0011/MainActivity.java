6bc4e3b7898c7cac3a0b9b97bc5f9f4eb409a3d2
seemani11 <thap0011@algonquinlive.com>
2021-09-21 14:36:19 -0400

    image button and text added

--- app/src/main/java/algonquin/cst2335/thap0011/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/thap0011/MainActivity.java
@@ -3,23 +3,34 @@ package algonquin.cst2335.thap0011;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private String rbstatus= "No";
+    private String cbstatus="No";
+     private String swtstatus= "No";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
         TextView mytext = findViewById(R.id.textview);
         Button mybutton = findViewById(R.id.mybutton);
         EditText myedit = findViewById(R.id.myedittext);
+
+        ImageView myimage = findViewById(R.id.logo_algonquin);
+        ImageButton imgbtn = findViewById( R.id.myimagebutton );
 
         CheckBox mycb = findViewById(R.id.cb);
         Switch mysw = findViewById(R.id.sw);
@@ -32,16 +43,23 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mycb.setOnCheckedChangeListener((btn,isChecked)->{
-            Toast.makeText(getApplicationContext(), "checkbox clicked", Toast.LENGTH_SHORT).show();
+            cbstatus = isChecked?"yes":"No";
+            Toast.makeText(getApplicationContext(), "Movie tonght? "+cbstatus, Toast.LENGTH_SHORT).show();
         });
 
         mysw.setOnCheckedChangeListener((btn,isChecked)->{
-            Toast.makeText(getApplicationContext(), "switch clicked", Toast.LENGTH_SHORT).show();
+            swtstatus = isChecked?"Yes":"No";
+            Toast.makeText(getApplicationContext(), "Coffee Tonght? "+swtstatus, Toast.LENGTH_SHORT).show();
         });
 
         myrb.setOnCheckedChangeListener((btn,isChecked)->{
-            Toast.makeText(getApplicationContext(), "Radiobutton clicked", Toast.LENGTH_SHORT).show();
+            rbstatus = isChecked ? "Yes":"No!";
+            Toast.makeText(getApplicationContext(), "agree on terms? ", Toast.LENGTH_SHORT).show();
         });
+
+          imgbtn.setOnClickListener( (View v)->{
+              Toast.makeText(getApplicationContext(), "height: "+ imgbtn.getHeight()+" width: "+ imgbtn.getWidth(),Toast.LENGTH_LONG).show();
+          });
     }
 
 }
